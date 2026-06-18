2e75896b67cb6009dcaabb1c9d5016ed1145f520
eirana <jonathanbiggestfan@gmail.com>
2026-06-19 08:19:41 +1000

    starving children

--- 08_contribute_to_the_campaign/scripts.js
+++ 08_contribute_to_the_campaign/scripts.js
@@ -16,5 +16,26 @@ function giveTenDollars() {
   document.getElementById("thankYouMessage").innerText = message;
 }
 
+function refundTenDollars() {
+  totalDonations = totalDonations - 10;
+
+  let message = "You better pay this back you cuck.";
+
+  if (totalDonations < 0){
+    totalDonations = 0
+    message = "Children are going to starve because of your selfish refunds you twat."
+  }
+
+ if (totalDonations >= 50 && totalDonations < 100) {
+    message = "🎉 We've hit our first campaign milestone!";
+  } else if (totalDonations >= 100) {
+    message = "🚀 Goal smashed! You powered this campaign!";
+  }
+
+  document.getElementById("totalDisplay").innerText = "Total raised: $" + totalDonations;
+  document.getElementById("thankYouMessage").innerText = message;
+}
+
+document.getElementById("refundButton").onclick = refundTenDollars
 document.getElementById("giveButton").onclick = giveTenDollars
 
